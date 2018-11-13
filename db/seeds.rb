fab8c7e8ff9e51fcc1d997f33b6b0576057887c3
jerome bodart <jerome.bodart@hei.yncrea.fr>
2018-11-13 16:57:45 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  h = {
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    date: Faker::Date.between(2.days.ago, Date.today),
+    text: Faker::Lorem.paragraphs
+  }
+  Article.new(h).save
+end
